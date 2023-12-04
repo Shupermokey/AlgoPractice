0f4e5aa615aa4cf21a7ca5086830964ee7316fe4
Shupermokey <a.e.sobieraj@gmail.com>
2023-12-04 00:20:44 -0600

    some coding algos to learn

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,10 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+  let val = str.split("").reverse().join("");
+
+  return val === str;
+}
 
 module.exports = palindrome;
