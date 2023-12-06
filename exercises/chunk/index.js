3da3f103f23203e9500a0a83f78ba8091745c92f
Shupermokey <a.e.sobieraj@gmail.com>
2023-12-06 02:08:30 -0600

    chunck completed

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,34 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  let chunked = [];
+  let i = 0;
+  while (i < array.length) {
+    chunked.push(array.slice(i, i + size));
+    i += size;
+  }
+  console.log("////////////////////////////////////////////////////////////");
+  console.log(chunked);
+  return chunked;
+}
 
 module.exports = chunk;
+
+//This worked but there is something better
+
+// let chunked = []
+// let arr = []
+// for(let i of array){
+//     if(arr.length < size){
+//       arr.push(i);
+//     }
+//     else {
+//       chunked.push(arr);
+//       arr = []
+//       arr.push(i);
+//     }
+
+// }
+// chunked.push(arr);
+// return chunked;
