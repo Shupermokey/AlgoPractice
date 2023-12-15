e3ee52f3859869c25df1cb03080038c14ca17311
Shupermokey <a.e.sobieraj@gmail.com>
2023-12-15 01:19:25 -0600

    steps finished

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,12 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n) {
+  for(let i = 1; i<=n; i++){
+    let step = "#".repeat(i);
+    let rest = " ".repeat(n-i);
+    console.log(step + rest);
+  }
+}
 
 module.exports = steps;
